ec8ccfc0fd3f37eea5a4e72ef6dc1f4c972cbef1
Teun van der Ploeg <29253@ma-web.nl>
2020-03-26 17:57:30 +0100

    achter grond
    achter grond maken

---
+++ javascript.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", MaakZon());
+window.addEventListener("load", MaakGrond());
+window.addEventListener("resize", windowRisize());
+
+function MaakZon() {
+  const c = document.getElementById("Canvas");
+  const ctx = c.getContext("2d");
+  c.height = window.innerHeight;
+  c.width = window.innerWidth;
+
+  ctx.beginPath();
+
+  ctx.arc(180, 320, 100, 0, 2 * Math.PI);
+  ctx.fillStyle = "rgba(255, 234, 0, 0.9)";
+  ctx.fill();
+
+  ctx.arc(180, 320, 125, 0, 2 * Math.PI);
+  ctx.fillStyle = "rgba(255, 234, 0, 0.5)";
+  ctx.fill();
+  ctx.arc(180, 320, 150, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function MaakGrond() {
+  const c = document.getElementById("Canvas1");
+  const ctx = c.getContext("2d");
+  c.height = window.innerHeight;
+  c.width = window.innerWidth;
+
+  ctx.fillStyle = "#00FF22";
+  ctx.fillRect(0, 320, 400, 110);
+
+  ctx.fillStyle = "#00A516";
+  ctx.fillRect(0, 430, 400, 110);
+
+  ctx.fillStyle = "#007D11";
+  ctx.fillRect(0, 540, 400, 110);
+}
+
+function windowRisize() {}
